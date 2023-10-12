8e2c630a810560fb9f6549e0bcb43ca3bc5fb13d
Olha Petkevych <olhapetkevych@outlook.com>
2023-10-12 20:05:34 +0300

    corrected error

--- practice/lecture-5/1.js
+++ practice/lecture-5/1.js
@@ -9,8 +9,9 @@ function printData(student) {
 function calculateAverageGrade(student) {
     if (student.grades.length < 3) {
         console.log("cannot calculate average grade - les than 3 grades")
+        return;//added
     }
-
+    
     sum = 0
 
     for (i = 0; i < student.grades.length; i++) {
