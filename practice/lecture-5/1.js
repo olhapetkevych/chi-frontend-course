6924e61d888eeeface28f0c50ba68be7ef990216
Olha Petkevych <olhapetkevych@outlook.com>
2023-10-12 19:59:14 +0300

    lesson 5 practice 1

---
+++ practice/lecture-5/1.js
@@ -0,0 +1,23 @@
+function createStudent(name, age, grades) {
+    return new Object(name, age, grades)
+}
+
+function printData(student) {
+    console.log("Name: "+student.name+"\nAge: "+student.age+"\nGrades: " + student.grades.toString())
+}
+
+function calculateAverageGrade(student) {
+    if (student.grades.length < 3) {
+        console.log("cannot calculate average grade - les than 3 grades")
+    }
+
+    sum = 0
+
+    for (i = 0; i < student.grades.length; i++) {
+        sum += student.grades[i]
+    }
+
+    average = sum / student.grades.length
+
+    console.log(average)
+}
