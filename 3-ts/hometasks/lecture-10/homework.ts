75d2a466556ba106bd76dff7292e10fba7d8b6eb
Olha Petkevych <olhapetkevych@outlook.com>
2023-10-31 23:16:39 +0200

    new project structure

---
+++ 3-ts/hometasks/lecture-10/homework.ts
@@ -0,0 +1,20 @@
+//Enums і Union
+enum Days {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+}
+
+
+//Дженерики
+
+
+
+//Type Aliases та Literal Types
+
+
+//Розширені можливості інтерфейсів
